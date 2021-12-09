4c07f853f5351cf1a5e82c23190a1e7ff875a489
DarwinEs <estudiodarwine3@gmail.com>
2021-12-09 10:00:51 -0600

    Subiendo

--- PROYECTOREACT/my-app/src/App.js
+++ PROYECTOREACT/my-app/src/App.js
@@ -1,9 +1,19 @@
 import React from "react"
+import Navbar from './components/NAVBAR/index'
+import Inicio from'./components/INICIO/index'
+import Footer from './components/FOOTER/index'
+import "../src/assets/estilo.css"
 
 export default function App () {
 return (
     <div>
-        Hola, aquí estoy yo
+        <Navbar/>
+        <Inicio/>
+        <Inicio/>
+        <Inicio/>
+        <Inicio/>
+        <Inicio/>
+        <Footer/>
     </div>
 )    
 }
